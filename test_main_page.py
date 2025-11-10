6a755d21a182449544c26bcbaa1869ee721c32ec
Olya <olya@Olyas-MacBook-Pro.local>
2025-11-10 11:34:00 +0200

    adding_informative_error_massage4.2_lesson6

--- test_main_page.py
+++ test_main_page.py
@@ -8,6 +8,7 @@ def test_guest_can_go_to_login_page(browser):
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
 
+#тест для проверки существования кнопки логина
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
