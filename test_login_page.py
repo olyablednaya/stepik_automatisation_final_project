93cc040c27c4f0f8ea61fd3b764912802da10f2d
Olya <olya@Olyas-MacBook-Pro.local>
2025-11-12 15:51:22 +0200

    Переход между страницами 4.2_lesson9

--- test_login_page.py
+++ test_login_page.py
@@ -4,6 +4,9 @@ from selenium.webdriver.common.by import By
 import time
 from .pages.login_page import LoginPage
 
+from .pages.main_page import MainPage
+
+
 #тест для проверки существования формы логина (проверяется существования полей и кнопки)
 def test_login_form_exist(browser):
     link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
@@ -24,6 +27,14 @@ def test_str_login_exist_in_current_url(browser):
     page = LoginPage(browser, link)
     page.open()
     page.should_be_login_url()
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
     
 
 
